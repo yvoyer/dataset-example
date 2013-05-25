009324e179a93a0a0dfb224e2270aeadc7d6a1ac
Yannick Voyer <yan.voyer@gmail.com>
2013-05-25 15:33:28 -0400

    Add tests for AbstractMapper

--- tests/Star/Component/DataSet/Test/Unit/Mapping/AbstractMapperTest.php
+++ tests/Star/Component/DataSet/Test/Unit/Mapping/AbstractMapperTest.php
@@ -16,8 +16,31 @@ namespace Star\Component\DataSet\Test\Unit\Mapping;
  */
 class AbstractMapperTest extends \PHPUnit_Framework_TestCase
 {
-    public function testToDo()
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Star\Component\DataSet\Mapping\AbstractMapper
+     */
+    private $mapper;
+
+    public function setUp()
     {
-        $this->markTestIncomplete('todo');
+        $this->mapper = $this->getMockForAbstractClass('Star\Component\DataSet\Mapping\AbstractMapper');
+    }
+
+    public function testShouldPopulateObject()
+    {
+        $value = uniqid();
+
+        $this->mapper
+            ->expects($this->once())
+            ->method('getMapping')
+            ->will($this->returnValue(array('id' => 'setId')));
+
+        $object = $this->getMock('\stdClass', array('setId'));
+        $object
+            ->expects($this->once())
+            ->method('setId')
+            ->with($value);
+
+        $this->mapper->populate($object, 'id', $value);
     }
 }
